009e648ecaf9961a84fc4a9234b64ac8767231b9
KIMHYEONJI13 <davithjk1310@gmail.com>
2024-08-05 21:53:43 +0900

    커밋

---
+++ src/test/java/com/mini/sample/MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
